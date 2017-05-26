db322f857147e50c41c98e7f2dd01e7ddb7e325d
Giperkot <gregster360@gmail.com>
2017-05-26 12:35:08 +0500

    Доработки главной страницы

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
-$(document).foundation()
+//$(document).foundation()
+
+$(document).ready(function () {
+/*    $('.cycle-slideshow').cycle({
+        speed: 600,
+        manualSpeed: 100,
+        prev: $(".invisto_slider__left_arrow"),
+        next: $(".invisto_slider__right_arrow"),
+        pauseOnHover: true,
+        fx: "scrollHorz",
+        slides: "> .invisto_slider__slide"
+
+    });*/
+    //$('.cycle-slideshow').cycle();
+});
